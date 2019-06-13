f8d48f5f3406ba629e8a052d9e837f19d405166f
Mareike Burg <mareike.burg01@stud.uni-goettingen.de>
2019-06-13 15:31:40 +0200

    removed stuff

--- script.py
+++ script.py
@@ -1,17 +1,6 @@
-import math
-import os
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who):
-    greeting = "hello, {}".format(who)
-    return greeting
-
 
 r = requests.get("https://google.com")
 print(r.status_code)
+print(r.ok)
+
